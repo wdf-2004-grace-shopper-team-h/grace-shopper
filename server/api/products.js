9305bf743e05e25a253d32242d9ce40c600f6f7d
Alex <aleksei_pavlov@yahoo.com>
2020-06-16 12:17:44 -0400

    api for all products added,installed 'chalk' for colorful messages. Added 'Product' model.

---
+++ server/api/products.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {green} = require('chalk')
+const Products = require('../db/models/products')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.status(200).json(products)
+    console.log(green('Fetch all products Success!'))
+  } catch (err) {
+    console.log(red("Can't fetch all products. "), err)
+    next(err)
+  }
+})
---
+++ server/db/models/products.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+module.exports = db.define('products', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  amount: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    defaultValue: 0
+  },
+  imgUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://www.countrystyle.com/wp-content/uploads/2016/09/SorryNoImageYet.png'
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+})
