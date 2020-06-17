054193e45bb327579eab112aca02c1d825c23968
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-06-17 15:12:32 -0400

    created cart reducer

---
+++ client/store/cart.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import history from '../history'
+
+const GET_ITEMS_IN_CART = 'GET_PRODUCTS_ITEMS_IN_CART'
+const ADD_TO_CART = 'ADD_TO_CART';
+
+const getItems = items => ({
+  type: GET_ITEMS_IN_CART,
+  items
+})
+
+const addItemToCart = productId => ({
+  type: ADD_TO_CART,
+  itemId: productId
+});
+
+const defaultItems = []
+
+export const fetchItems = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/cart')
+    dispatch(getItems(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+
+export default (state = defaultItems, action) => {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return action.items
+    case ADD_TO_CART:
+      return [...state, action.itemId]
+    default:
+      return state
+  }
+}
