59bc078c3a95352d135445168f05cdc5e88aa40d
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-06-18 15:06:47 -0400

    continued work on cart redux store and trying to implement test specs

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,38 +1,50 @@
 import axios from 'axios'
 import history from '../history'
 
-const GET_ITEMS_IN_CART = 'GET_ITEMS_IN_CART';
-const ADD_TO_CART = 'ADD_TO_CART';
+const GET_ITEMS_IN_CART = 'GET_ITEMS_IN_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
 
 export const getItems = items => ({
   type: GET_ITEMS_IN_CART,
   items
 })
 
-export const addItemToCart = productId => ({
+export const addItemToCart = (productId, quantity) => ({
   type: ADD_TO_CART,
-  itemId: productId
-});
+  item: {
+    itemId: productId,
+    quantity
+  }
+})
 
 const defaultItems = []
-
-export const fetchItems = (id) => async dispatch => {
+//what is this trying to get?
+//to get all cart items when a user get to their cart page, it should display all items.
+export const fetchItems = () => async dispatch => {
   try {
-    const { data } = await axios.get(`/api/cart/${id}`);
+    const {data} = await axios.get(`/api/cart`)
     dispatch(getItems(data))
   } catch (error) {
     console.error(error)
   }
 }
 
-
+//will grab the id from the product and user quantity on the react end to send here and update the database cart model to include the new item.
+export const putItemInCart = (productId, userQuantity) => async dispatch => {
+  try {
+    const res = await axios.put(`/api/cart/${productId}`, userQuantity)
+    dispatch(addItemToCart(productId, userQuantity))
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default (state = defaultItems, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
       return action.items
     case ADD_TO_CART:
-      return [...state, action.itemId]
+      return [...state, action.item]
     default:
       return state
   }
