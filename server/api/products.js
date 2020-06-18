f1b6a2bb0d140d269bcf262814b5e78cac5b1ac9
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-06-18 15:24:47 -0400

    added single product route

--- server/api/products.js
+++ server/api/products.js
@@ -13,3 +13,18 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+app.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Products.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(product)
+    console.log(green(`Fetch product with id: ${req.params.id} Success!`))
+  } catch (error) {
+    console.log(red(`Can't fetch product with id: ${req.params.id}`))
+    next(error)
+  }
+})
